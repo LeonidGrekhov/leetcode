34c44952b476d05ac14ac0f7e71e613c16316d00
Leonid Grekhov <lgrekhov98@gmail.com>
2023-06-14 12:53:48 -0700

    ptr solution

--- TwoPointer/ValidPalindrome125.py
+++ TwoPointer/ValidPalindrome125.py
@@ -39,10 +39,26 @@ class Solution:
                 newStr += character.lower()
         return newStr == newStr[::-1]
     
-    
+    def isPalindromePtr(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+        s = s.upper()
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+                continue
+            if not s[right].isalnum():
+                right -= 1
+                continue
 
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
  
 
 s1 = Solution()
 print(s1.isPalindrome(s = "A man, a plan, a canal: Panama"))
 print(s1.isPalindrome(s = "A man, a plan, a canal: "))
+print(s1.isPalindromePtr(s = "A man, a plan, a canal: Panama"))
+print(s1.isPalindromePtr(s = "A man, a plan, a canal: "))
