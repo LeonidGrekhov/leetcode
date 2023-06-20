c1d0ee6afa206b342cda15bfd1bb9d5ac9eba387
Leonid Grekhov <lgrekhov98@gmail.com>
2023-06-20 11:18:08 -0700

    solution

--- Sliding window/LongestSubstringWithoutRepeatingCharacters3.py
+++ Sliding window/LongestSubstringWithoutRepeatingCharacters3.py
@@ -30,3 +30,18 @@ s consists of English letters, digits, symbols and spaces.
 '''
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        left = 0
+        result = 0
+        for right in range(len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+            charSet.add(s[right])
+            result = max(result, right - left + 1)
+        return result
+    
+s1 = Solution()
+print(s1.lengthOfLongestSubstring(s="abcabcbb"))
+print(s1.lengthOfLongestSubstring(s="bbbbb"))
+print(s1.lengthOfLongestSubstring(s="pwwkew"))
