1d2657f1468a587abf9bccc83aecc93e9e7950de
Leonid Grekhov <lgrekhov98@gmail.com>
2023-06-22 09:21:36 -0700

    OrderedSet

--- Sliding window/LongestSubstringWithoutRepeatingCharacters3.py
+++ Sliding window/LongestSubstringWithoutRepeatingCharacters3.py
@@ -28,9 +28,10 @@ Constraints:
 0 <= s.length <= 5 * 104
 s consists of English letters, digits, symbols and spaces.
 '''
+from sortedcollections import OrderedSet
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        charSet = set()
+        charSet = OrderedSet()
         left = 0
         result = 0
         for right in range(len(s)):
