89b30fb94cd2f0fde642e25b3c85fa69cafcfc33
Leonid Grekhov <lgrekhov98@gmail.com>
2023-06-23 09:46:27 -0700

    solution

--- Linked List/RemoveDuplicatesFromSortedList83.py
+++ Linked List/RemoveDuplicatesFromSortedList83.py
@@ -19,3 +19,54 @@ The number of nodes in the list is in the range [0, 300].
 -100 <= Node.val <= 100
 The list is guaranteed to be sorted in ascending order.
 '''
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        while cur:
+            while cur.next and cur.next.val == cur.val:
+                cur.next = cur.next.next
+            cur = cur.next
+        return head
+
+# Function to create a linked list from a list
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def addNode(self,node):
+        node = ListNode(node)
+        if self.head is None:
+            self.head = node
+        else:
+            self.tail.next = node
+        self.tail = node
+    
+    def printList(self):
+        if self.head is None:
+            print("no nodes")
+        else:
+            temp = self.head
+            while temp:
+                if temp.next:
+                    end = "->"
+                else:
+                    end = "\n"
+                print(temp.val,end=end)
+                temp = temp.next
+list1 = LinkedList()
+list1.addNode(1)
+list1.addNode(1)
+list1.addNode(2)
+list1.addNode(3)
+list1.addNode(3)
+list1.printList()
+s1 = Solution()
+list1.head = s1.deleteDuplicates(list1.head)
+list1.printList()
