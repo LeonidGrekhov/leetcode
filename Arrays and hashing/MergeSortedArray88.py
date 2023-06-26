7a7cb84e028cedba5daefc79cebea2aacbb59ac7
Leonid Grekhov <lgrekhov98@gmail.com>
2023-06-26 09:06:29 -0700

    for loop implementation

--- Arrays and hashing/MergeSortedArray88.py
+++ Arrays and hashing/MergeSortedArray88.py
@@ -48,7 +48,11 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        if m == 0 and n == 0:
+            print('empty')
+            return
         last  = m + n - 1
+        
 
         while m > 0 and n > 0:
             if nums1[m - 1]>nums2[n - 1]:
@@ -61,6 +65,26 @@ class Solution:
         while n > 0: # edge case of first element in num2 being larger than num1
             nums1[last] = nums2[n - 1]
             n, last = n - 1, last - 1
+    def merge2(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if m == 0 and n == 0:
+            print('empty')
+            return
+
+        last = m + n - 1
+        m -= 1
+        n -= 1
+        
+        for i in range(last, -1, -1):
+            if n == -1 or (m >= 0 and nums1[m] > nums2[n]):
+                nums1[i] = nums1[m]
+                m -= 1
+            else:
+                nums1[i] = nums2[n]
+                n -= 1
+             
 
 s1 = Solution()
 nums1= [2,2,3,0,0,0]
@@ -68,3 +92,11 @@ print(nums1)
 
 s1.merge(nums1, m = 3, nums2 = [1,5,6], n = 3)
 print(nums1)
+s1.merge(nums1=[1,2,3], m = 0, nums2 = [4,5,6], n = 0)
+
+nums1= [2,2,3,0,0,0]
+print(nums1)
+
+s1.merge2(nums1, m = 3, nums2 = [1,5,6], n = 3)
+print(nums1)
+s1.merge2(nums1=[1,2,3], m = 0, nums2 = [4,5,6], n = 0)
