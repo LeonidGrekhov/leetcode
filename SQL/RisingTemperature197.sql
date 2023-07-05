a9fa5859798cc82059cd221683e22dd4138e08ed
Leonid Grekhov <lgrekhov98@gmail.com>
2023-07-05 12:11:49 -0700

    description and solution

---
+++ SQL/RisingTemperature197.sql
@@ -0,0 +1,60 @@
+/*
+197. Rising Temperature
+Easy
+
+SQL Schema
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+*/
+CREATE TABLE IF NOT EXISTS Weather (id INT, recordDate DATE, temperature INT);
+TRUNCATE TABLE Weather;
+INSERT INTO Weather (id, recordDate, temperature) VALUES ('1', '2015-01-01', '10');
+INSERT INTO Weather (id, recordDate, temperature) VALUES ('2', '2015-01-02', '25');
+INSERT INTO Weather (id, recordDate, temperature) VALUES ('3', '2015-01-03', '20');
+INSERT INTO Weather (id, recordDate, temperature) VALUES ('4', '2015-01-04', '30');
+
+SELECT t1.id
+FROM Weather AS t1, Weather AS t2
+WHERE t1.temperature > t2.temperature AND 
+DATEDIFF(t1.recordDate, t2.recordDate)=1;
