87f11604dcc14bc2fadb0897dd27c787093af037
Leonid Grekhov <lgrekhov98@gmail.com>
2023-07-07 12:24:59 -0700

    description and solution

---
+++ SQL/TriangleJudgement610.sql
@@ -0,0 +1,53 @@
+/*
+610. Triangle Judgement
+Easy
+
+SQL Schema
+Table: Triangle
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+(x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+ 
+
+Write an SQL query to report for every three line segments whether they can form a triangle.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+Example 1:
+
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
+*/
+
+CREATE TABLE IF NOT EXISTS Triangle (x INT, y INT, z INT);
+TRUNCATE TABLE Triangle;
+INSERT INTO Triangle (x, y, z) VALUES ('13', '15', '30');
+INSERT INTO Triangle (x, y, z) VALUES ('10', '20', '15');
+
+SELECT x,y,z, 
+CASE 
+WHEN x+y<=z THEN 'No'
+WHEN y+z<=x THEN 'No'
+WHEN z+x<=y THEN 'No'
+ELSE 'Yes' END AS triangle FROM triangle;
